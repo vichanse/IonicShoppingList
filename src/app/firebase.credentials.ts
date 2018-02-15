f8b70c38e7eee364a30c9bc82eef9e70eb32f265
Vicky <vicky.nseng@gmail.com>
2018-02-15 14:25:45 +0100

    Adding Angularfire2

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDrSS1Owvr6F7S7FKwSre74GkOs7FiBShE",
+    authDomain: "ionicshoppinglist-c4041.firebaseapp.com",
+    databaseURL: "https://ionicshoppinglist-c4041.firebaseio.com",
+    projectId: "ionicshoppinglist-c4041",
+    storageBucket: "",
+    messagingSenderId: "597217325201"
+  };
