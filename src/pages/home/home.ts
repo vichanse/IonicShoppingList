b4bb73642d5b888b5857150b02c1dcbb7b7c83a8
Vicky <vicky.nseng@gmail.com>
2018-02-16 04:53:49 +0100

    Displaying shopping item

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { ShoppingListService } from "../../services/shopping-list/shopping-list.service";
+import { Observable } from "rxjs/Observable";
+import { Item } from "../../models/item/item.model";
 
 @IonicPage()
 @Component({
@@ -8,7 +11,22 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  shoppingList$: Observable<Item[]>;
+
+  constructor(
+    public navCtrl: NavController,
+    private shoppingListService: ShoppingListService
+  ) {
+    this.shoppingList$ = this.shoppingListService
+      .getShoppingList()
+      .snapshotChanges()
+      .map(
+        changes => {
+          return changes.map(c => ({
+            key: c.payload.key, ...c.payload.val()
+          }))
+        }
+      )
 
   }
 
