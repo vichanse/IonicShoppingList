d9540c7439a335841bdc410a6f478b2a2cfe57f5
Vicky <vicky.nseng@gmail.com>
2018-02-16 05:30:01 +0100

    Adding a ToastService

--- src/pages/add-shopping-item/add-shopping-item.ts
+++ src/pages/add-shopping-item/add-shopping-item.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams} from 'ionic-angular';
 import { Item } from "../../models/item/item.model";
 import { ShoppingListService } from "../../services/shopping-list/shopping-list.service";
+import { ToastService } from "../../services/toast/toast.service";
 
 /**
  * Generated class for the AddShoppingItemPage page.
@@ -26,7 +27,8 @@ export class AddShoppingItemPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    private shoppingListService: ShoppingListService
+    private shoppingListService: ShoppingListService,
+    private toastService: ToastService
   ) {
   }
 
@@ -36,6 +38,7 @@ export class AddShoppingItemPage {
 
   addItem(item: Item) {
     this.shoppingListService.addItem(item).then(ref => {
+      this.toastService.show(`${item.name} added!`)
       this.navCtrl.setRoot('HomePage', { key: ref.key });
     });
   }
--- src/pages/edit-shopping-item/edit-shopping-item.ts
+++ src/pages/edit-shopping-item/edit-shopping-item.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Item } from "../../models/item/item.model";
 import { ShoppingListService } from "../../services/shopping-list/shopping-list.service";
+import { ToastService } from "../../services/toast/toast.service";
 
 
 
@@ -17,7 +18,8 @@ export class EditShoppingItemPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    private shoppingListServie: ShoppingListService
+    private shoppingListServie: ShoppingListService,
+    private toastService: ToastService
   ) {
   }
 
@@ -27,6 +29,7 @@ export class EditShoppingItemPage {
 
   saveItem(item: Item) {
     this.shoppingListServie.editItem(item).then(() => {
+      this.toastService.show(`${item.name} saved!`)
       this.navCtrl.setRoot('HomePage');
     })
   }
