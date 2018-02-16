cf10b93d3bb4dc9bf7cc0e279e54759d8229bd34
Vicky <vicky.nseng@gmail.com>
2018-02-16 05:19:31 +0100

    Editing shopping items

---
+++ src/pages/edit-shopping-item/edit-shopping-item.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Item } from "../../models/item/item.model";
+import { ShoppingListService } from "../../services/shopping-list/shopping-list.service";
+
+
+
+@IonicPage()
+@Component({
+  selector: 'page-edit-shopping-item',
+  templateUrl: 'edit-shopping-item.html',
+})
+export class EditShoppingItemPage {
+
+  item: Item;
+  
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private shoppingListServie: ShoppingListService
+  ) {
+  }
+
+  ionViewWillLoad() {
+    this.item = this.navParams.get('item');
+  }
+
+  saveItem(item: Item) {
+    this.shoppingListServie.editItem(item).then(() => {
+      this.navCtrl.setRoot('HomePage');
+    })
+  }
+
+}
--- src/services/shopping-list/shopping-list.service.ts
+++ src/services/shopping-list/shopping-list.service.ts
@@ -19,4 +19,8 @@ export class ShoppingListService {
         return this.shoppingListRef.push(item);
     }
 
+    editItem(item: Item) {
+        return this.shoppingListRef.update(item.key, item);
+    }
+
 }
